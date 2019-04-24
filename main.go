8cc432db6451a2edd29b68e2dc04ba9ed20c4998
liam humphreys <liam.humphreys1989@gmail.com>
2019-04-24 11:16:24 +0100

    colours! buddy: Erin

--- main.go
+++ main.go
@@ -17,19 +17,24 @@ func main() {
 	log.Println("Beginning deployment")
 
 	chartDir, chartValues, appName, targetEnv, appVersion := parseCmdLineFlags()
-	log.Printf(
-		`Preparing to deploy using these variables:
-			chartDir: %s
-			chartValues: %s
-			appName: %s
-			targetEnv: %s
-			appVersion: %s`,
-		chartDir, chartValues, appName, targetEnv, appVersion)
+	log.Println("Preparing to deploy using these variables:")
+	log.Printf("\tchartDir: \033[32m%s\033[97m", chartDir)
+	log.Printf("\tchartValues: \033[32m%s\033[97m", chartValues)
+	log.Printf("\tappName: \033[32m%s\033[97m", appName)
+	log.Printf("\ttargetEnv: \033[32m%s\033[97m", targetEnv)
+	log.Printf("\tappVersion: \033[32m%s\033[97m", appVersion)
 
+	log.Println("Accessing kubernetes..")
 	kubernetes := kubernetesCoreV1()
+	log.Println("Determining deploy colour..")
 	deployColour := determineDeployColour(targetEnv, appName, kubernetes)
-	log.Printf("Determined deploy colour: %s", deployColour)
+	log.Printf("Determined deploy colour: \033[32m%s\033[97m", deployColour)
 
+	log.Printf("Deploying %s from %s to: %s-%s-%s", appVersion,
+		chartDir,
+		targetEnv,
+		deployColour,
+		appName)
 	// if err != nil {
 	// 	panic(err.Error())
 	// }
@@ -71,23 +76,23 @@ func parseCmdLineFlags() (string, string, string, string, string) {
 	invalidFlags := false
 	if *chartDir == "" || !IsDirectory(*chartDir) {
 		invalidFlags = true
-		log.Printf("Invalid chart-dir: %s, must be %s", *chartDir, chartDirUsage)
+		log.Printf("Invalid \033[32mchart-dir\033[97m: \033[31m%s\033[97m, must be \033[33m%s\033[97m", *chartDir, chartDirUsage)
 	}
 	if *appName == "" || !IsValidAppName(*appName) {
 		invalidFlags = true
-		log.Printf("Invalid app-name: %s, must be %s", *appName, appNameUsage)
+		log.Printf("Invalid \033[32mapp-name\033[97m: \033[31m%s\033[97m, must be \033[33m%s\033[97m", *appName, appNameUsage)
 	}
 	if *appVersion == "" || !IsValidAppVersion(*appVersion) {
 		invalidFlags = true
-		log.Printf("Invalid app-version: %s, must be %s", *appVersion, appVersionUsage)
+		log.Printf("Invalid \033[32mapp-version\033[97m: \033[31m%s\033[97m, must be \033[33m%s\033[97m", *appVersion, appVersionUsage)
 	}
 	if *targetEnv == "" || !IsValidTargetEnv(*targetEnv) {
 		invalidFlags = true
-		log.Printf("Invalid target-env: %s, must be %s", *targetEnv, targetEnvUsage)
+		log.Printf("Invalid \033[32mtarget-env\033[97m: \033[31m%s\033[97m, must be \033[33m%s\033[97m", *targetEnv, targetEnvUsage)
 	}
 	if chartValues == "" || !FileExists(chartValues) {
 		invalidFlags = true
-		log.Printf("Expected to find chart values yaml at: %s, but found nothing.", chartValues)
+		log.Printf("Expected to find chart values yaml at: \033[31m%s\033[97m, but found nothing.", chartValues)
 	}
 	if invalidFlags {
 		panic("Invalid flag supplied, see log.")
@@ -101,13 +106,13 @@ func homeDir() string {
 
 func kubernetesCoreV1() v1.CoreV1Interface {
 	homeDir := homeDir()
-	log.Printf("Using home dir: %s", homeDir)
+	log.Printf("Using home dir: \033[32m%s\033[97m", homeDir)
 
 	kubeConfigPath := KubeConfigPath(homeDir)
-	log.Printf("Derived kubeconfig path: %s", kubeConfigPath)
+	log.Printf("Derived kubeconfig path: \033[32m%s\033[97m", kubeConfigPath)
 
 	config := KubeConfig(kubeConfigPath)
-	log.Printf("Successfully found kubeconfig at: %s", kubeConfigPath)
+	log.Printf("Successfully found kubeconfig at: \033[32m%s\033[97m", kubeConfigPath)
 
 	log.Println("Creating kubectl clientset..")
 	clientset, err := kubernetes.NewForConfig(config)
@@ -120,15 +125,15 @@ func kubernetesCoreV1() v1.CoreV1Interface {
 
 func determineDeployColour(targetEnv, appName string, kubernetes v1.CoreV1Interface) string {
 	offlineServiceName := OfflineServiceName(targetEnv, appName)
-	log.Printf("Looking for the colour selector of the offline service: %s", offlineServiceName)
+	log.Printf("Looking for the colour selector of the offline service: \033[32m%s\033[97m", offlineServiceName)
 	service, err := kubernetes.Services("default").Get(offlineServiceName, metav1.GetOptions{})
 	if err != nil || service == nil {
-		log.Printf("Offline service %s was not found, defaulting..", offlineServiceName)
+		log.Printf("Offline service \033[32m%s\033[97m was not found, defaulting..", offlineServiceName)
 	} else if service.Spec.Selector == nil || len(service.Spec.Selector) == 0 {
 		if _, ok := service.Spec.Selector["colour"]; !ok {
-			log.Printf("Offline service %s was found but it had no colour selector, defaulting..", offlineServiceName)
+			log.Printf("Offline service \033[32m%s\033[97m was found but it had no colour selector, defaulting..", offlineServiceName)
 		} else {
-			log.Printf("Offline service %s was found but it had no selectors, defaulting..", offlineServiceName)
+			log.Printf("Offline service \033[32m%s\033[97m was found but it had no selectors, defaulting..", offlineServiceName)
 		}
 	}
 	return ServiceSelectorColour(service, err)
